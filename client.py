cf67e29e76e3ba3eaeebdf1890a76e5b4791df28
guofuwei <2625406970@qq.com>
2021-09-25 23:27:53 +0800

    fix login/register bug

--- client.py
+++ client.py
@@ -13,75 +13,81 @@ class Client():
         theclient_socket.connect((server_ip,server_port))
         self.theclient_socket=theclient_socket
     
-    def send_msg(self):
+    def send_msg(self,username):
         while True:
-            send_data=input('')
+            send_data=input(username+':')
             if send_data=='EXIT':
-                self.theclient_socket.close()
+                self.theclient_socket.send('EXIT'.encode('utf-8'))
                 break
-            self.theclient_socket.send(send_data.encode('utf-8'))
+            self.theclient_socket.send(username+':'+send_data.encode('utf-8'))
 
-    def recv_msg(self):
+    def recv_msg(self,username):
         while True:
             try:
-                recv_data=self.theclient_socket.recv(1024)
-                print(recv_data.decode('utf-8'))
+                recv_data=self.theclient_socket.recv(1024).decode('utf-8')
+                print('\r'+recv_data)
+                print(username+':')
             except:
                 break
 
     def login(self):
         servertype=int(input('请选择注册还是登录(登录:1,注册:2):'))
         if servertype==1:
-            username='',password=''
             self.theclient_socket.send('LOGIN'.encode('utf-8'))
             print('现在您正在登录')
-            print('请输入您的用户名:%s' %username)
-            print('请输入您的密码:%s' %password)
-            self.theclient_socket.send(json.dumps({'username':username,'password':password}))
-            recv_flag=self.theclient_socket.recv(1024).decode('utf-8')
-            if recv_flag=='OK':
+            username=input('请输入您的用户名:')
+            password=input('请输入您的密码:')
+            self.theclient_socket.send(json.dumps({'username':username,'password':password}).encode('utf-8'))
+            recv_flag=json.loads(self.theclient_socket.recv(1024).decode('utf-8'))
+            if recv_flag.get('code')=='OK':
                 print('登录成功!')
-                return 'OK'
+                return recv_flag
             else:
                 print(recv_flag)
                 time.sleep(1)
                 os.system('cls')
 
         elif servertype==2:
-            username='',password1='',password2=''
             self.theclient_socket.send('REGISTER'.encode('utf-8'))
             print('现在为您注册一个新用户')
-            print('请输入您的用户名:%s' %username)
-            print('请输入您的密码:%s' %password1)
-            print('请再次输入您的密码:%s' %password2)
+            username=input('请输入您的用户名:')
+            password1=input('请输入您的密码:')
+            password2=input('请再次输入您的密码:')
             self.theclient_socket.send(json.dumps({'username':username,'password1':password1,
-            'password2':password2}))
-            recv_flag=self.theclient_socket.recv(1024).decode('utf-8')
-            if recv_flag=='OK':
+            'password2':password2}).encode('utf-8'))
+            recv_flag=json.loads(self.theclient_socket.recv(1024).decode('utf-8'))
+            if recv_flag.get('code')=='OK':
                 print('注册成功!')
-                return 'OK'
+                return recv_flag
             else:
                 print(recv_flag)
                 time.sleep(1)
                 os.system('cls')
 
     def run(self):
+        print('*****欢迎来到网络聊天室v1.0*****')
         while True:
             print('您还未登录，请先登录!')
             flag=self.login()
-            if flag=='OK':
+            if flag.get('code')=='OK':
+                username=flag.get('msg')
                 break
+        # print(flag)
+        # print(username)
         print('*******您已经进入菜单界面*******')
         print('请选择您要求的服务类型:')
-        print('输入1：进入公告聊天室')
+        print('输入1：进入公共聊天室')
+        print('输入0:提出程序')
 
         servernum=int(input('请输入选择的功能:'))
-        if servernum==1:
+        if servernum==0:exit()
+        elif servernum==1:
+            self.theclient_socket.send('GROUP_CHAT'.encode('utf-8'))
             os.system('cls')
             print('*****您已成功进入公共聊天室*****')
             print('*****输入EXIT退出*****')
-            t1=threading.Thread(target=self.recv_msg)
-            t2=threading.Thread(target=self.send_msg)
+            t1=threading.Thread(target=self.recv_msg,args=(username,))
+            t2=threading.Thread(target=self.send_msg,args=(username,))
             t1.start()
             t2.start()
 
--- server.py
+++ server.py
@@ -3,6 +3,9 @@ import threading
 import pymysql
 import settings
 import json
+import hashlib
+
+
 conn = pymysql.connect(
     host=settings.MYSQL_CONFIG['HOST'],
     user=settings.MYSQL_CONFIG['USER'],
@@ -15,12 +18,14 @@ class Client_Handle():
     def __init__(self,client_socket):
         self.client_socket=client_socket
 
-    def recv_msg(self):
+    def recvmsg_group_view(self):
         global clients_list
         while True:
             try:
                 recv_data=self.client_socket.recv(1024).decode('utf-8')
-                if recv_data:
+                if recv_data=='EXIT':
+                    pass
+                elif recv_data:
                     send_data=recv_data
                     for client in clients_list:
                         client.send(send_data.encode('utf-8'))
@@ -29,24 +34,27 @@ class Client_Handle():
                 self.client_socket.close()
                 break
 
-    def run(self):
+    def login_register_view(self):
         is_login=self.client_socket.recv(1024).decode('utf-8')
         if is_login=='LOGIN':
-            json_obj=json.loads(self.client_socket.recv(1024))
+            json_obj=json.loads(self.client_socket.recv(1024).decode('utf-8'))
             username=json_obj.get('username')
             password=json_obj.get('password')
+            p_m=hashlib.md5()
+            p_m.update(password.encode())
+            password_m=p_m.hexdigest()
             sql="select * from user_profile where username=%s and password=%s"
             try:
-                cursor1.execute(sql,[username,password])
+                cursor1.execute(sql,[username,password_m])
                 data=cursor1.fetchone
                 if data:
-                    self.client_socket.send('OK'.encode('utf-8'))
+                    self.client_socket.send(json.dumps({'code':'OK','msg':username}).encode('utf-8'))
                 else:
                     self.client_socket.send('用户名或密码错误'.encode('utf-8'))
             except:
                 pass
         elif is_login=='REGISTER':
-            json_obj=json.loads(self.client_socket.recv(1024))
+            json_obj=json.loads(self.client_socket.recv(1024).decode('utf-8'))
             username=json_obj.get('username')
             password1=json_obj.get('password1')
             password2=json_obj.get('password2')
@@ -55,17 +63,28 @@ class Client_Handle():
             elif password1!=password2:
                 self.client_socket.send('两次密码不一致，注册失败'.encode('utf-8'))
             else:
-                pass
+                p_m=hashlib.md5()
+                p_m.update(password1.encode())
+                password_m=p_m.hexdigest()
+                sql='''insert into user_profile(username,password) values(%s,%s)'''
+                cursor1.execute(sql,[username,password_m])
+                conn.commit()
+                self.client_socket.send(json.dumps({'code':'OK','msg':username}).encode('utf-8'))
 
-            
-
-
+    def run(self):
+        # 首先接受登录或注册的请求
+        self.login_register_view()
+        # 开始处理需要的业务请求
+        view_flag=self.client_socket.recv(1024).decode('utf-8')
+        if view_flag=='GROUP_CHAT':
+            self.recvmsg_group_view()
 
 
 
 def main():
     server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    # 防止意外关闭导致端口被占无法开启
+    # server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
     server_socket.bind(('',7788))
     server_socket.listen(128)
     global client_list
@@ -76,6 +95,7 @@ def main():
         client_socket,client_addrs=server_socket.accept()
         clients_list.append(client_socket)
         client_handle=Client_Handle(client_socket)
+        client_handle.run()
     server_socket.close()
 
 
